d6277c808fbf621a3bca011fc591e44a9b271b90
Jo├úo Paulo <joaopaulo102003@gmail.com>
2025-08-11 08:13:05 -0300

    remove auto_migrations

--- obj/Debug/net8.0/app-tarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/app-tarefas.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app-tarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4e2addfcae06aa314f32d0fa585183d458c5ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e4fd356498e851f1a5a01b0b5cc2bd92636b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("app-tarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app-tarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
